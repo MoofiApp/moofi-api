fde5bcc74060119ab75c8f37a783c5c3f60a7ad2
Moofi App <moofinance.app@protonmail.com>
2021-10-14 16:16:23 +0200

    Added single moonfarm vault

--- src/api/stats/moonriver/getMoonfarmApys.js
+++ src/api/stats/moonriver/getMoonfarmApys.js
@@ -3,6 +3,7 @@ import { MOON_LPF } from '../../../constants';
 
 const MasterChefAbi = require('../../../abis/MasterChef.json');
 const pools = require('../../../data/moonriver/moonfarmLpPools.json');
+const singlePools = require('../../../data/moonriver/moonfarmPools.json');
 const {
   moonfarm,
 } = require('../../../../packages/address-book/address-book/moonriver/platforms/moonfarm');
@@ -13,6 +14,7 @@ const getMoonfarmApys = async () =>
     tokenPerBlock: 'moonfarmPerBlock',
     hasMultiplier: true,
     pools: pools,
+    singlePools,
     oracleId: 'MOON',
     oracle: 'tokens',
     decimals: '1e18',
