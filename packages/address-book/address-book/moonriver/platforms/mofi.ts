c9efb34567a51949be734b7a898204b50c3d94a2
Moofi App <moofinance.app@protonmail.com>
2021-10-27 21:06:46 +0200

    Updated vault owners in address book

--- packages/address-book/address-book/moonriver/platforms/mofi.ts
+++ packages/address-book/address-book/moonriver/platforms/mofi.ts
@@ -1,10 +1,11 @@
 const deployer = '0x8784279bdB1b634d5bEf86C92262D1775248aEE0';
 const feeReward = '0x6A90b35784c716dB7B269f2Eda2fB7e7C164Bb06';
 const devMultisig = '0x5DF70B08A3377AC0B9F5291Ad156Ab6e30622166';
+const timelock = '0xe1F1eAFdD20f2135176A303A56AA8Ccbac4135d8';
 
 export const mofi = {
   strategyOwner: devMultisig,
-  vaultOwner: devMultisig,
+  vaultOwner: timelock,
   keeper: deployer,
   rewarder: feeReward,
   treasurer: feeReward,
